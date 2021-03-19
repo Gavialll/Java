772707329442ffd20628056d9849f0a0968801fc
andriudutko@gmail.com <andriudytko@gmail.com>
2021-03-19 21:01:19 +0200

    Map

--- src/Map/DZ3/Animal.java
+++ src/Map/DZ3/Animal.java
@@ -3,11 +3,13 @@ package Map.DZ3;
 public class Animal {
     private String nameAnimal;
     private String typeAnimal;
+    private boolean animalBool = true;
 
     public Animal(String nameAnimal, String typeAnimal) {
         this.nameAnimal = nameAnimal;
         this.typeAnimal = typeAnimal;
     }
+
 
     public String getNameAnimal() {
         return nameAnimal;
@@ -23,5 +25,13 @@ public class Animal {
 
     public void setTypeAnimal(String typeAnimal) {
         this.typeAnimal = typeAnimal;
+    }
+
+    public boolean isAnimalBool() {
+        return animalBool;
+    }
+
+    public void setAnimalBool(boolean animalBool) {
+        this.animalBool = animalBool;
     }
 }
--- src/Map/DZ3/Start.java
+++ src/Map/DZ3/Start.java
@@ -1,7 +1,5 @@
 package Map.DZ3;
 
-import Map.DZ2.Car;
-
 import java.util.*;
 
 public class Start {
@@ -14,11 +12,13 @@ public class Start {
         Реалізувати консольне меню(НЕ обов’язково), таким чином щоб можна було:
                 ● Додати людину до клубу
                 ● Додати тваринку до людини
-                ● Видалити(забрати) тваринку від людини
+                ● Видалити тваринку від людини
                 ● Видалити людину з клубу
                 ● Вивести на екран зооклуб
                 ● Вийти з програми
         Використати для побудови меню Switch.*/
+
+        Person person = new Person("Ron", 21);
 
         Map<Person, List<Animal>> zooClubMap = new LinkedHashMap<>();
         Scanner scanner = new Scanner(System.in);
@@ -27,62 +27,75 @@ public class Start {
                                new Animal("Рекс","Пес"),
                                new Animal("Пуцик","Заєць"),
                                new Animal("Карат","Кінь")));
+
+
         while (true) {
 
-                System.out.println("Меню");
-                System.out.println("1 : Додати людину до клубу                  |");
-                System.out.println("2 : Додати тваринку до людини               |");
-                System.out.println("3 : Видалити(забрати) тваринку від людини   |");
-                System.out.println("4 : Видалити людину з клубу                 |");
-                System.out.println("5 : Вивести на екран зооклуб                |");
-                System.out.println("6 : Вийти з програми                        |");
-                System.out.println("------------------------");
-                System.out.print("Введіть дію: ");
+            System.out.println("Меню");
+            System.out.print(" 1: Додати людину до клубу|");
+            System.out.print(" 2: Додати тваринку до людини|");
+            System.out.print(" 3: Видалити тваринку від людини|");
+            System.out.print(" 4: Видалити людину з клубу|");
+            System.out.print(" 5: Вивести на екран зооклуб|");
+            System.out.println(" 6: Вийти з програми|");
+            System.out.println("------------------------");
+            System.out.print("Введіть дію: ");
 
-                int num = 0;
-                if (scanner.hasNextInt()) {
-                    num = scanner.nextInt();
+            int num = 0;
+            if (scanner.hasNextInt()) {
+                num = scanner.nextInt();
+            }
+
+            switch (num) {
+
+                case 1: {
+                    System.out.println("Імя людини: ");
+                    zooClubMap.put(person, new ArrayList<>());
+                    System.out.println(person.getName() + " " + person.getAge());
+                    break;
                 }
-
-                switch (num) {
-
-                    case 1: {
-                        Person person = new Person("Ron", 21);
-                        System.out.println("Імя людини: ");
-                        zooClubMap.put(person , new ArrayList<>());
-
-
-                        break;
-                    }
-                    case 2: {
-                        //System.out.println("Name");
-                        //String name = scanner.next();
-                        //System.out.println("Age");
-                        //Integer age = scanner.nextInt();
-
-                        Animal animal = new Animal("topik", "Dog");
-
-                        break;
-                    }
-                    case 3: {
-                        System.out.println("info");
-                        Animal animal = (Animal) zooClubMap.get(new Person("Ron", 21));
-                        System.out.println(animal.getNameAnimal());
-                        break;
-                    }
-                    case 4: {
-                        break;
-                    }
-                    case 5: {
-                        for (Map.Entry<Person, List<Animal>> entry : zooClubMap.entrySet()) {
-                            Iterator<List<Animal>> iterator = zooClubMap.values().iterator();
-                            while (iterator.hasNext()){
-                                System.out.println(iterator.next().get(0).getNameAnimal());
+                case 2: {
+                    List<Animal> list = zooClubMap.get(person);
+                    System.out.println("add animal");
+                    list.add(listAnimal.get(0));
+                            zooClubMap.put(person, list);
+                            listAnimal.remove(0);
+                    break;
+                }
+                case 3: {
+                    List <Animal> list = zooClubMap.get(person);
+                    Iterator<Animal> iterator = list.iterator();
+                    if(iterator.hasNext()) {
+                        System.out.println("delete Animal");
+                        String animal = scanner.next();
+                        for (int i = 0; i < list.size(); i++) {
+                            if (iterator.next().getNameAnimal().equals(animal)) {
+                                listAnimal.add(list.get(i));
+                                iterator.remove();
                             }
                         }
-                        break;
                     }
+                    break;
                 }
+                case 4: {
+                    List<Animal> listPerson = zooClubMap.get(person);
+                    listAnimal.addAll(listPerson);
+                    zooClubMap.remove(person);
+                    break;
+                }
+                case 5: {
+                    for (Map.Entry<Person, List<Animal>> entry : zooClubMap.entrySet()) {
+                        for (Animal animal : entry.getValue()) {
+                            System.out.println(animal.getNameAnimal());
+                        }
+                    }
+                    System.out.println();
+                    for (Animal a : listAnimal){
+                            System.out.println(a.getNameAnimal());
+                        }
+                    break;
+                }
+            }
         }
     }
 }
